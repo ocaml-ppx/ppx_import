8f025d2945086405f3ba77441db5e3e93d9bf584
Peter Zotov <whitequark@whitequark.org>
2014-08-17 14:17:08 +0400

    Build with recent OPAM.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -3,10 +3,6 @@ open Ocamlbuild_plugin
 let () = dispatch (
   function
   | After_rules ->
-    flag ["ocaml"; "compile"; "safe_string"] & A"-safe-string";
-    flag ["ocaml"; "compile"; "use_import"] &
-      S[A"-ppx"; A"src/ppx_import.native"];
-    flag ["ocaml"; "ocamldep"; "use_import"] &
-      S[A"-ppx"; A"src/ppx_import.native"];
-
+    flag ["ocaml"; "ocamldep"; "use_import"] & S[A"-ppx"; A"src/ppx_import.native"];
+    flag ["ocaml"; "compile";  "use_import"] & S[A"-ppx"; A"src/ppx_import.native"]
   | _ -> ())
