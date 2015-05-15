ad0fc28f06699bbfdb11735b0e62c23b4cd4b1d4
whitequark <whitequark@whitequark.org>
2015-05-15 03:21:26 +0300

    Use libexec .install field (fixes #5).

--- pkg/build.ml
+++ pkg/build.ml
@@ -5,7 +5,7 @@
 let () =
   Pkg.describe "ppx_import" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
-    Pkg.bin ~auto:true "src/ppx_import" ~dst:"../lib/ppx_import/ppx_import";
+    Pkg.libexec ~auto:true "src/ppx_import";
     Pkg.doc "README.md";
     Pkg.doc "LICENSE.txt";
     Pkg.doc "CHANGELOG.md"; ]
