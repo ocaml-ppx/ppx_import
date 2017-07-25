0e08dc8e298e3e643aea4dacae4af06dd71b1791
Ralf Vogler <ralf.vogler@gmail.com>
2017-07-25 15:04:33 +0200

    install all the files, now resulting driver complains: ~~~ (cd _build/default && ./.ppx/ppx_import+ppx_deriving/ppx.exe --dump-ast -o test.pp.ml --impl test.ml) Sys_error("--impl: No such file or directory") ~~~

--- pkg/build.ml
+++ pkg/build.ml
@@ -8,6 +8,11 @@ let ocamlbuild =
 let () =
   Pkg.describe "ppx_import" ~builder:(`Other (ocamlbuild, "_build")) [
     Pkg.lib "pkg/META";
+    Pkg.lib "src/ppx_import.o";
+    Pkg.lib "src/ppx_import.cmo";
+    Pkg.lib "src/ppx_import.cmx";
+    Pkg.lib "src/ppx_import.cma";
+    Pkg.lib "src/ppx_import.cmxs";
     Pkg.libexec ~auto:true "src/ppx_import";
     Pkg.doc "README.md";
     Pkg.doc "LICENSE.txt";
