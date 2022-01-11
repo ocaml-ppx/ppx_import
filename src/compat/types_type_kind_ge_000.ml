1f1c169018b5bca6dc3b118754277f391d3a5b51
tatchi <corentin.leruth@gmail.com>
2022-01-11 17:05:17 +0100

    rename type_kind_407 to type_kind_412

--- src/compat/types_type_kind_ge_000.ml
+++ src/compat/types_type_kind_ge_000.ml
@@ -1,3 +1,3 @@
-type type_kind_407 = Types.type_kind
+type type_kind_412 = Types.type_kind
 
-let migrate_type_kind : Types.type_kind -> type_kind_407 = fun x -> x
+let migrate_type_kind : Types.type_kind -> type_kind_412 = fun x -> x
